bf6601ffdd595d45eadaff8c9a0aec9b379cf79b
chase <smithc0222@gmail.com>
2017-01-31 19:03:36 +0000

    resize templates for phones

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY='something'
     SQLALCHEMY_DATABASE_URI='sqlite:///lockout.db'
-    UPLOAD_FOLDER = '/home/OhmsFlas/mysite/trinitylockout/static/lockout'
+    UPLOAD_FOLDER = '/home/chasesmith/mysite/lockout/static/lockout'
     ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
 class PythonAnywhereConfig(BaseConfig):
-    UPLOAD_FOLDER='/home/OhmsFlaw/mysite/trinitylockout/static/lockout'
+    UPLOAD_FOLDER='/home/chasesmith/mysite/lockout/static/lockout'
