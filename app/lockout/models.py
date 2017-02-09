d1b03d4198aeffb0bcafbcf53414b53f73117ed6
smithc0222 <smithc0222@gmail.com>
2017-02-09 14:03:18 -0500

    split models into multiple status tabes

--- app/lockout/models.py
+++ app/lockout/models.py
@@ -7,20 +7,8 @@ class Lockout(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     lockout_number=db.Column(db.String(10), nullable=False)
     lockout_description=db.Column(db.String(200), nullable=False)
-    lockout_author = db.relationship('User', backref=db.backref('lockout_author', lazy='dynamic'))
-    user_id=db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
     lockout_file=db.Column(db.String(20), nullable=True)
     pid_file=db.Column(db.String(20), nullable=True)
-    open_status=db.Column(db.Boolean(), default=True)
-    closed_status=db.Column(db.Boolean(), default=False)
-    implemented_by= db.relationship('User', backref=db.backref('implemented_by', lazy='dynamic'))
-    implemented_status=db.Column(db.Boolean(), default=False)
-    accepted_by = db.relationship('User', backref=db.backref('accepted_by', lazy='dynamic'))
-    released_by = db.relationship('User', backref=db.backref('released_by', lazy='dynamic'))
-    accepted_status=db.Column(db.Boolean(), default=False)
-    work_status=db.Column(db.Boolean(), default=False)
-    released_status=db.Column(db.Boolean(),default=False)
-    closed_by=db.relationship('User', backref=db.backref('pulled_by', lazy='dynamic'))
     goggles=db.Column(db.Boolean(), default=False)
     faceshield=db.Column(db.Boolean(),default=False)
     fullface=db.Column(db.Boolean(), default=False)
@@ -33,19 +21,13 @@ class Lockout(db.Model):
     tyrex=db.Column(db.Boolean(),default=False)
     rubberboots=db.Column(db.Boolean(),default=False)
     sar=db.Column(db.Boolean(),default=False)
-    date=db.Column(db.DateTime)
     ppe=db.Column(db.String(50))
 
-
-    def __init__(self, lockout_number, lockout_description, lockout_author, goggles, faceshield,
+    def __init__(self, lockout_number, lockout_description, goggles, faceshield,
         fullface, dustmask, leathergloves, saranax, nitrilegloves, chemicalsuit, chemicalgloves,
-        tyrex, rubberboots, sar, ppe, date=None):
+        tyrex, rubberboots, sar, ppe):
         self.lockout_number=lockout_number
         self.lockout_description=lockout_description
-        if date is None:
-            date = datetime.utcnow()
-        self.date = date
-        self.lockout_author=lockout_author
         self.goggles=goggles
         self.faceshield=faceshield
         self.fullface=fullface
@@ -62,6 +44,116 @@ class Lockout(db.Model):
 
     def __repr__(self):
         return 'Lockout: {}'.format(self.lockout_number)
+
+class Open_Table(db.Model):
+
+    __tablename__="open_table"
+    id=db.Column(db.Integer, primary_key=True)
+    open_status=db.Column(db.Boolean(), default=True)
+    created_by = db.relationship('User', backref=db.backref('created_by', lazy='dynamic'))
+    user_id=db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    lockout=db.relationship('Lockout', backref=db.backref('open_lockout', lazy='dynamic'))
+    lockout_id=db.Column('lockout_id', db.Integer, db.ForeignKey('lockout.id'))
+    date=db.Column(db.DateTime)
+
+    def __init__(self, open_status, created_by, lockout, date=None):
+        self.open_status=open_status
+        self.created_by=created_by
+        self.lockout=lockout
+        if date is None:
+            date = datetime.utcnow()
+        self.date=date
+
+    def __repr__(self):
+        return 'Open by: {} | Lockout Desc: {}'.format(self.created_by, self.lockout.lockout_description)
+
+class Implemented_Table(db.Model):
+
+    __tablename__="implemented_table"
+    id=db.Column(db.Integer, primary_key=True)
+    implemented_status=db.Column(db.Boolean(), default=True)
+    implemented_by= db.relationship('User', backref=db.backref('implemented_by', lazy='dynamic'))
+    user_id=db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    lockout=db.relationship('Lockout', backref=db.backref('implemented_lockout', lazy='dynamic'))
+    lockout_id=db.Column('lockout_id', db.Integer, db.ForeignKey('lockout.id'))
+    date=db.Column(db.DateTime)
+
+    def __init__(self, implemented_status, implemented_by, lockout, date=None):
+        self.implemented_status=implemented_status
+        self.implemented_by=implemented_by
+        self.lockout=lockout
+        if date is None:
+            date = datetime.utcnow()
+        self.date=date
+
+    def __repr__(self):
+        return 'Implemented by: {} | Lockout Desc: {}'.format(self.implemented_by, self.lockout.lockout_description)
+
+class Accepted_Table(db.Model):
+
+    __tablename__="accepted_table"
+    id=db.Column(db.Integer, primary_key=True)
+    accepted_status=db.Column(db.Boolean(), default=True)
+    accepted_by = db.relationship('User', backref=db.backref('accepted_by', lazy='dynamic'))
+    user_id=db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    lockout=db.relationship('Lockout', backref=db.backref('accepted_lockout', lazy='dynamic'))
+    lockout_id=db.Column('lockout_id', db.Integer, db.ForeignKey('lockout.id'))
+    date=db.Column(db.DateTime)
+
+    def __init__(self, accepted_status, accepted_by, lockout, date=None):
+        self.accepted_status=accepted_status
+        self.accepted_by=accepted_by
+        self.lockout=lockout
+        if date is None:
+            date = datetime.utcnow()
+        self.date=date
+
+    def __repr__(self):
+        return 'Accepted by: {} | Lockout Desc: {}'.format(self.accepted_by, self.lockout.lockout_description)
+
+class Released_Table(db.Model):
+
+    __tablename__="released_table"
+    id=db.Column(db.Integer, primary_key=True)
+    released_status=db.Column(db.Boolean(), default=True)
+    released_by = db.relationship('User', backref=db.backref('released_by', lazy='dynamic'))
+    user_id=db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    lockout=db.relationship('Lockout', backref=db.backref('released_lockout', lazy='dynamic'))
+    lockout_id=db.Column('lockout_id', db.Integer, db.ForeignKey('lockout.id'))
+    date=db.Column(db.DateTime)
+
+    def __init__(self, released_status, released_by, lockout, date=None):
+        self.released_status=released_status
+        self.released_by=released_by
+        self.lockout=lockout
+        if date is None:
+            date = datetime.utcnow()
+        self.date=date
+
+    def __repr__(self):
+        return 'Released by: {} | Lockout Desc: {}'.format(self.released_by, self.lockout.lockout_description)
+
+class Cleared_Table(db.Model):
+
+    __tablename__="cleared_table"
+    id=db.Column(db.Integer, primary_key=True)
+    cleared_status=db.Column(db.Boolean(), default=True)
+    cleared_by = db.relationship('User', backref=db.backref('cleared_by', lazy='dynamic'))
+    user_id=db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+    lockout=db.relationship('Lockout', backref=db.backref('cleared_lockout', lazy='dynamic'))
+    lockout_id=db.Column('lockout_id', db.Integer, db.ForeignKey('lockout.id'))
+    date=db.Column(db.DateTime)
+
+    def __init__(self, cleared_status, cleared_by, lockout, date=None):
+        self.cleared_status=cleared_status
+        self.cleared_by=cleared_by
+        self.lockout=lockout
+        if date is None:
+            date = datetime.utcnow()
+        self.date=date
+
+    def __repr__(self):
+        return 'Cleared by: {} | Lockout Desc: {}'.format(self.cleared_status, self.lockout.lockout_description)
 
 class Lockout_Line(db.Model):
 
--- db_create.py
+++ db_create.py
@@ -1,7 +1,7 @@
 #create db and populate with data.
 from app import db
 from app.auth.models import User
-from app.lockout.models import Lockout,Lockout_Line
+from app.lockout.models import Lockout,Lockout_Line, Open_Table
 
 db.drop_all()
 db.create_all()
@@ -12,7 +12,8 @@ db.session.add(User(username='bfurby', first_name='Belinda',last_name='Furby',po
 
 user1=db.session.query(User).first()
 db.session.commit()
-lockout1=Lockout(lockout_number='02022017-1', lockout_description='Test lockout', lockout_author=user1,
+
+lockout1=Lockout(lockout_number='02022017-1', lockout_description='Test lockout',
                 goggles=0, faceshield=0, fullface=0, dustmask=0,
                 leathergloves=0, saranax=0, nitrilegloves=0,
                 chemicalsuit=0, chemicalgloves=0, tyrex=0,
@@ -20,6 +21,9 @@ lockout1=Lockout(lockout_number='02022017-1', lockout_description='Test lockout'
 db.session.add(lockout1)
 db.session.commit()
 
+open1=Open_Table(open_status=1, created_by=user1, lockout=lockout1, date=None)
+db.session.add(open1)
+db.session.commit()
 
 lockoutline1=Lockout_Line('V929-00','Test Valve','open','close',lockout1)
 db.session.add(lockoutline1)
